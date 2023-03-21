5bcbe6624d222df07d5d2c18ac26e5db306809f1
Braden <braden@bradenn.com>
2023-03-21 12:40:31 -0700

    Added data-tag ui element composed of the divider and tag.

---
+++ client/src/components/Divider.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+
+
+</script>
+
+<template>
+  <div class="mx-1" style="width: 1px; height: 2rem; background-color: rgba(255,255,255,0.2);"></div>
+
+</template>
+
+<style scoped>
+.element-fg-disabled > div {
+  filter: brightness(80%) contrast(10%);
+  /*opacity: 0.3;*/
+}
+</style>
---
+++ client/src/components/Tag.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+
+interface TagProps {
+  name: string
+  desc?: string
+  value: string
+  active?: string
+  disabled?: boolean
+}
+
+
+const props = defineProps<TagProps>()
+
+</script>
+
+<template>
+  <div :class="props.disabled?'element-fg-disabled':''" class="element-fg">
+    <div class="d-flex flex-column align-items-start ">
+      <div class="label-c5 label-o4">
+        {{ props.name }}
+      </div>
+      <div :class="props.active" class="label-c4 label-o5 label-mono">
+        {{ props.value }}
+      </div>
+
+    </div>
+
+  </div>
+
+</template>
+
+<style scoped>
+.element-fg-disabled > div {
+  filter: brightness(80%) contrast(10%);
+  /*opacity: 0.3;*/
+}
+</style>
